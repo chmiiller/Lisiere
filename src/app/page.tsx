b34085f99924cbfa7ac12c973969abde2c043e97
Carlos Zinato <carlosh.lz@hotmail.com>
2025-01-12 23:13:09 +0100

    moves footer code to a separate component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/Footer';
 import Image from 'next/image';
 
 export default function Home() {
@@ -9,25 +10,7 @@ export default function Home() {
         width={384}
         height={1}
       />
-      <div className="bg-slate-50 h-14 p-3 w-full flex flex-row">
-        <div className='flex flex-col flex-grow'>
-          <p className='text-gray-800 text-5xs font-medium'>ISO1600 26mm f1.5 1/14s</p>
-          <p className='text-gray-800 text-5xs font-extralight'>28 Dec, 2024</p>
-        </div>
-        <div className='flex flex-row items-center justify-end w-20'>
-          <Image
-            src={'/logo/sony.svg'}
-            width={60}
-            height={60}
-            alt='sony logo'
-          />
-          <div className='bg-gray-300 h-full rounded w-px ml-1' />
-        </div>
-        <div className='flex flex-grow flex-col items-end'>
-          <p className='text-gray-800 text-5xs font-medium'>Apple iPhone 14</p>
-          <p className='text-gray-800 text-5xs font-extralight'>back dual wide camera 5.7mm f1.5</p>
-        </div>
-      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+
+export default function Footer() {
+  return (
+    <div className="bg-slate-50 h-14 p-3 w-full flex flex-row">
+        <div className='flex flex-col flex-grow'>
+          <p className='text-gray-800 text-5xs font-medium'>ISO1600 26mm f1.5 1/14s</p>
+          <p className='text-gray-800 text-5xs font-extralight'>28 Dec, 2024</p>
+        </div>
+        <div className='flex flex-row items-center justify-end w-20'>
+          <Image
+            src={'/logo/sony.svg'}
+            width={60}
+            height={60}
+            alt='sony logo'
+          />
+          <div className='bg-gray-300 h-full rounded w-px ml-1' />
+        </div>
+        <div className='flex flex-grow flex-col items-end'>
+          <p className='text-gray-800 text-5xs font-medium'>Apple iPhone 14</p>
+          <p className='text-gray-800 text-5xs font-extralight'>back dual wide camera 5.7mm f1.5</p>
+        </div>
+      </div>
+  );
+}
