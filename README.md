05449408c737c6257a9f988e848117c98bbbe9b5
Carlos Zinato <carlosh.lz@hotmail.com>
2025-02-13 23:46:09 +0100

    finally testing parseExif

--- README.md
+++ README.md
@@ -16,18 +16,10 @@ This project uses [`next/font`](https://nextjs.org/docs/app/building-your-applic
 
 ## To Do
 
-[] Docs
 [] Tests
+[] Docs
 [] NextJS MetaData
 [] ESLint rules
 [] General style
 [] Custom footer style
 [] Custom footer background color
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
--- src/tests/utils.test.tsx
+++ src/tests/utils.test.tsx
@@ -1,11 +1,19 @@
-import { exifTimestampAsDate, formatDateForPicker, getValidFileType, shortString } from '../utils';
-import { expect, test } from 'vitest';
+import {
+  exifTimestampAsDate,
+  formatDateForPicker,
+  getValidFileType,
+  shortString,
+  parseExif,
+} from '../utils';
+import { expect, expectTypeOf,test } from 'vitest';
+import * as fs from 'fs/promises';
+import path from 'path';
 
 test('converting EXIF timestamp to JS Date', () => {
   const originalTimestamp = "2025:01:27 12:33:11";
   const timestampAsDate = exifTimestampAsDate(originalTimestamp);
   const expectedDate = new Date(2025, 0, 27, 12, 33, 11);
-
+  expectTypeOf(expectedDate).toEqualTypeOf<Date>()
   expect(timestampAsDate.getFullYear()).equal(expectedDate.getFullYear());
   expect(timestampAsDate.getMonth()).equal(expectedDate.getMonth());
   expect(timestampAsDate.getDate()).equal(expectedDate.getDate());
@@ -50,3 +58,51 @@ test('if a long string is properly shortened', () => {
   const expectedString = 'f/5.22';
   expect(short).toEqual(expectedString);
 });
+
+test('if parseExif returns the correct object when an image is passed', async() => {
+  const imagePath = path.resolve(__dirname, '../../public/sample.jpg');
+  const imageBuffer = await fs.readFile(imagePath).then(buffer => buffer.buffer) as ArrayBuffer;
+  const mockFile = new File([imageBuffer], 'image_with_exif.jpg', { type: 'image/jpeg' });
+
+  const validExifData = {
+    brandLogo: "/logos/sony.png",
+    brandName: "Sony",
+    focalLength: 70,
+    fstop: "f/2.8",
+    iso: 320,
+    lensModel: "24-70mm F2.8 DG DN | Art 019",
+    model: "ILCE-7",
+    speed: "1/1000",
+  };
+  const exifData = await parseExif(mockFile);
+  expect(exifData.brandLogo).toEqual(validExifData.brandLogo);
+  expect(typeof exifData.brandLogo).toBe('string');
+  
+  expect(exifData.brandName).toEqual(validExifData.brandName);
+  expect(typeof exifData.brandName).toBe('string');
+  
+  expect(exifData.focalLength).toEqual(validExifData.focalLength);
+  expect(typeof exifData.focalLength).toBe('number');
+  
+  expect(exifData.fstop).toEqual(validExifData.fstop);
+  expect(typeof exifData.fstop).toBe('string');
+  
+  expect(exifData.iso).toEqual(validExifData.iso);
+  expect(typeof exifData.iso).toBe('number');
+  
+  expect(exifData.lensModel).toEqual(validExifData.lensModel);
+  expect(typeof exifData.lensModel).toBe('string');
+  
+  expect(exifData.model).toEqual(validExifData.model);
+  expect(typeof exifData.model).toBe('string');
+  
+  expect(exifData.speed).toEqual(validExifData.speed);
+  expect(typeof exifData.speed).toBe('string');
+  expect(typeof exifData.timestamp).toBe('object');
+})
+
+test('if parseExif returns error when passing a text document', async() => {
+  const blob = new Blob(['this is a text file']);
+  const mockFile = new File([blob], 'document.txt', { type: 'text/plain' });
+  await expect(parseExif(mockFile)).rejects.toThrowError('Invalid image format');
+})
